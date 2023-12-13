945d795622e0352eb077c8464726da1bcac2d793
hrypasato <hrypasato@gmail.com>
2023-12-13 17:27:40 -0500

    fix: null & undefined

--- src/api.js
+++ src/api.js
@@ -470,7 +470,7 @@ router.post("/producto", (req, res) => {
 router.get("/list", (req, res) => {  
   const { name, fabricante } = req.query;
 
-  if(fabricante) {
+  if(fabricante !== null && fabricante !== undefined) {
     const result = listaFabricantes.filter(item => 
       item.toLowerCase()
       .includes(fabricante.toLocaleLowerCase()));
